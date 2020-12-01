a083610ca4e70a83123219624bfb1e14d4aafdd4
bineanshi <bineanshi@hotmail.com>
2020-12-01 14:51:57 +0800

    bin

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-CC=x86_64-linux-musl-gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o dist/snatch -ldflags "-linkmode external -extldflags -static" cron.go
-CC=x86_64-linux-musl-gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o dist/BingWallpaperServer -ldflags "-linkmode external -extldflags -static" main.go
+CC=x86_64-linux-musl-gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w"  -o ./bin/BingWallpaperServer
