9991f5f1b685bac03cb4e4b4e10f8e22efcaee02
bineanshi <bineanshi@hotmail.com>
2020-12-01 15:21:13 +0800

    route template view

--- routers/web.go
+++ routers/web.go
@@ -2,22 +2,14 @@ package routers
 
 import (
 	. "SnatchBingWallpaper/controllers"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"os"
-	"path/filepath"
 )
 
 func InitRouter() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	pwd, err := os.Getwd()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
 
-	router.LoadHTMLGlob(filepath.Join(pwd,"templates/*/*.tmpl"))
+	router.LoadHTMLGlob("templates/*/*.tmpl")
 	router.GET("/:date", List)
 
 	return router
