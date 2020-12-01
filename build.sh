e758940c4dbf517349df7f74e682735f7bf531a3
bineanshi <bineanshi@hotmail.com>
2020-12-01 15:18:11 +0800

    bin

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CC=x86_64-linux-musl-gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w"  -o ./bin/BingWallpaperServer
+CC=x86_64-linux-musl-gcc CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o ./bin/BingWallpaperServer -ldflags "-s -w" main.go
